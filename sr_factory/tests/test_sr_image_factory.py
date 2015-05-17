4faff45c79601a30d96388bf5cae3edf31883386
Shaofeng Chen <shaofeng_2010@hotmail.com>
2015-05-16 19:13:35 -0700

    fix unit test

--- sr_factory/tests/test_sr_image_factory.py
+++ sr_factory/tests/test_sr_image_factory.py
@@ -8,12 +8,12 @@ class TestSRImageFactory(unittest.TestCase):
 
     def test_create_sr_image_from(self):
         sr_image = SRImageFactory.create_sr_image_from("test_data/babyface_4.png")
-        self.assertIsNotNone(sr_image.get_image())
+        self.assertIsNotNone(sr_image.get_data())
 
     def test_create_sr_image(self):
         image = Image.open("test_data/babyface_4.png")
         sr_image = SRImageFactory.create_sr_image(image)
-        self.assertIsNotNone(sr_image.get_image())
+        self.assertIsNotNone(sr_image.get_data())
 
 if __name__ == "__main__":
     unittest.main()
